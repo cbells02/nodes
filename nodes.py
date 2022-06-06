a5b026112ece92d05d69a0e49e40f06f17ea706a
Charles <cbells002@gmail.com>
2022-06-06 15:51:51 -0500

    Finished creating first node with objects!

--- nodes.py
+++ nodes.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, value, link_node = None):
+    def __init__(self, value, link_node=None):
 
         self.value = value
         self.link_node = link_node
@@ -12,3 +12,17 @@ class Node:
 
     def set_link_node(self, link_node):
         self.link_node = link_node
+
+
+yacko = Node("likes to yak")
+wacko = Node("has a penchant for hoarding snacks")
+dot = Node("enjoys spending time in movie lots")
+
+dot.set_link_node(wacko)
+yacko.set_link_node(dot)
+
+dots_data = yacko.get_link_node().get_value()
+wackos_data = dot.get_link_node().get_value()
+
+print(dots_data)
+print(wackos_data)
